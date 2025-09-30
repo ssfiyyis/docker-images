1f3913874defc5a0075074586d877e20b3c2ec9e
seMgh5Yty8px <seMgh5Yty8px>
2025-09-30 16:18:26 +0900

    add training scenario

--- llm/dummy_train.py
+++ llm/dummy_train.py
@@ -1,7 +1,7 @@
 from nemo.collections import llm
 import nemo_run as run
 
-if __name__ == "__main__":
+def llama3_8b():
     # 1) レシピを構成（実行はまだ行われない）
     #    ※ ローカル最小実行を想定して GPU=1 にしてあります。
     #       マシンに応じて num_gpus_per_node を増減してください。
@@ -37,3 +37,37 @@ if __name__ == "__main__":
         executor=executor,
         name="llama3_8b_pretraining_dummy10_local",
     )
+
+def qwen2_1p5b():
+        # 1) レシピを構成（実行はまだ行われない）
+    #    ※ ローカル最小実行を想定して GPU=1 にしてあります。
+    #       マシンに応じて num_gpus_per_node を増減してください。
+    recipe = llm.qwen2_1p5b.pretrain_recipe(
+        name="qwen2_1p5b_dummy10",
+        dir="./checkpoints_qwen2_1p5b_dummy10",
+        num_nodes=1,
+        num_gpus_per_node=8,   # 例: 1GPU実行（要メモリ）
+    )
+
+    recipe.trainer.max_steps = 10
+    recipe.trainer.enable_checkpointing = False
+    recipe.trainer.log_every_n_steps = 1
+
+    recipe.model.config.seq_length = 512
+    recipe.data.seq_length = 512
+    recipe.data.global_batch_size = 8         # 総バッチ（DP×MBの積）
+    recipe.data.micro_batch_size = 1          # 1GPUあたりのマイクロバッチ
+
+    # 3) 実行エグゼキュータ（ローカル）
+    executor = run.LocalExecutor()
+
+    run.run(
+        recipe,
+        executor=executor,
+        name="qwen2_1p5b_pretraining_dummy10_local",
+    )
+
+
+
+if __name__ == "__main__":
+    qwen2_1p5b()
